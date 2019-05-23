48b5c20c91c635879b26031b60a69d65289c2584
Suat ATAN PhD <suatatan@gmail.com>
2019-05-23 14:02:14 +0300

    Update harcama_karti.js
    Harcama kartı modülü üzerinden yevmiye kaytı yapılabiliyor.

--- src/myjs/harcama_karti.js
+++ src/myjs/harcama_karti.js
@@ -12,11 +12,13 @@ var harcama_karti = {
     var doc_id = state.currentUser.email
     state.db.collection("harcama_kartlari").doc(doc_id).set(yollanacak_data).then(doc => {
       console.log("Yeni kullanıcı oluşturuldu: ", doc_id);
+      return true
     }).then(() => {
       //this.sonuc_goster()
     }).catch(err => {
       console.log(err)
       alert("Bir hata oluştu:" + err)
+      return false
     })
     //---- Yeni harcama kartı ekle bitiş ---
   },
@@ -29,11 +31,29 @@ var harcama_karti = {
           } else {
             // doc.data() will be undefined in this case
             console.log("Böyle bir kullanıcı veritabanında yok! Haydi yenisini oluşturalım");
-            harcama_karti.ekle(state) // izah this bu modül içinden çağırırken de bu şekilde çağırmalısın
+            if(harcama_karti.ekle(state)){ // izah this bu modül içinden çağırırken de bu şekilde çağırmalısın
+              return true
+            }else{
+              return false
+            }
+
           }
         }).catch(function(error) {
           console.log("Kolleksiyondan veritabanı çekilirken hata oluştu:", error);
+          return false
         });
+  },
+
+  yevmiye_ekle(state,yollanacak_data){
+        statedb.collection("harcama_kartlari/" + state.currentUser.email + "/harcamalar").add(yollanacak_data).then(doc => {
+          console.log("Yevmiye kaydı eklendi ID: ", doc.id);
+        }).then(() => {
+          //this.sonuc_goster()
+          return true
+        }).catch(err => {
+          console.log(err)
+          return false
+        })
   }
 
 }
